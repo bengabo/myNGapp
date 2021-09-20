8f52919439b7cc3fc8f4c213ca2041657d11d84a
Gaboury <benjamin.gaboury@accenture.com>
2021-09-20 12:49:56 +0200

    routing and service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpService } from './http.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'myNGapp';
+  title = 'Hello, how are you?';
+  jsonvalue = {
+    id: 1,
+    name: "Wamwam"
+  }
+
+  newDate = new Date();
+
+  constructor(private httpService: HttpService){}
+
+  handleEvent(){
+    this.httpService.getRequest('https://jsonplaceholder.typicode.com/todos/1');
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TruncatePipe } from './truncate.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TruncatePipe,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
